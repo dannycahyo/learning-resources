dedc6c3bcb57bec37168f97f746324b22952fd7a
dannycahyo <dannydwicahyono@gmail.com>
2023-02-24 09:18:16 +0700

    feat: adjust composed function

--- frontend-masters/functional-light-v3/composition/ex.js
+++ frontend-masters/functional-light-v3/composition/ex.js
@@ -14,27 +14,29 @@ function half(x) {
 }
 
 function pipe(...funcs) {
-  return function composed(v) {
+  return function piped(v) {
     return funcs.reduce((prevFunc, fn) => {
       return fn(prevFunc);
     }, v);
   };
 }
 
-function compose(...funcs) {
-  return pipe(...funcs.reverse());
-}
+const compose = (...funcs) => {
+  return function composed(v) {
+    return funcs.reduceRight((prevFunc, fn) => {
+      return fn(prevFunc);
+    }, v);
+  };
+};
 
 var f1 = compose(increment, decrement);
 var f2 = pipe(decrement, increment);
-var f3 = compose(decrement, double, increment, half);
 var f4 = pipe(half, increment, double, decrement);
 var f5 = compose(increment);
 var f6 = pipe(increment);
 
 console.log(f1(3) === 3);
 console.log(f1(3) === f2(3));
-console.log(f3(3) === 4);
 console.log(f3(3) === f4(3));
 console.log(f5(3) === 4);
 console.log(f5(3) === f6(3));
